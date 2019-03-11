eacaa052cc40c313b2c03ca0e8c74112ae54af99
Blake Stoner <stonerblake@gmail.com>
2019-03-11 16:25:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,10 +17,9 @@ function yourAnimal() {
 var n = 449
 
 function add2(n) {
+   const two = 2
   return n + two
-
   // Feel free to move things around!
-  const two = 2
 }
 
 var funkyFunction = function() {
